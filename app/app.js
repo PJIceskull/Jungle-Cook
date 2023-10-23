8019159bc89bea49069d927d15e45ddd4414fd0a
Pierce Issah <PJIceskull2003@gmail.com>
2023-10-23 13:51:05 -0400

    add change route function

---
+++ app/app.js
@@ -0,0 +1,31 @@
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  console.log(hashTag + " " + pageID);
+
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      console.log(pageID, "page not found");
+      alert(pageID + " page not found.");
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    }).fail(function () {
+      alert(pageID + " page not found.");
+    });
+  }
+}
+
+function initURLListener() {
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
+
+$(document).ready(function () {
+  initURLListener();
+});
